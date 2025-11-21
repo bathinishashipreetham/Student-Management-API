99234a5a785011ae359099abd18f580f14a72f73
bathinishashipreetham <bathinishashipreetham@gmail.com>
2025-11-21 15:15:55 +0530

    Initialize ASP.NET Core application with Swagger and DB

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using StudentManagement.API.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// In-Memory DB
+builder.Services.AddDbContext<StudentDbContext>(options =>
+    options.UseInMemoryDatabase("StudentDb"));
+
+var app = builder.Build();
+
+// Configure middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
