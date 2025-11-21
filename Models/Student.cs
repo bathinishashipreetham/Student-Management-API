25ea1751e875efe5541e72ee11ea14e7e54f55dc
bathinishashipreetham <bathinishashipreetham@gmail.com>
2025-11-21 16:32:32 +0530

    Update Student.cs

--- Models/Student.cs
+++ Models/Student.cs
@@ -3,8 +3,8 @@ namespace StudentManagement.API.Models
     public class Student
     {
         public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
+        public string Name { get; set; }
         public int Age { get; set; }
-        public string Email { get; set; } = string.Empty;
+        public string Email { get; set; }
     }
 }
